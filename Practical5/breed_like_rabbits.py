1da8a1d09d5704456d188c136da75fcc6d6f49b1
xiaoran <xiaoran.22@intl.zju.edu.cn>
2023-03-15 08:32:17 +0100

    rabbits

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,3 +1,9 @@
+#What is the number of the generation?
+# The first generation has 2 rabbits
+# if x is smaller than 100
+#  the number of generation will add 1
+# if x is larger than 100
+# print the generation number that has more than 100 rabbits
 n = 1
 x = 2
 while x<100:
