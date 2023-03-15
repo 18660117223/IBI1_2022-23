5251a77a0f67f1530bc4c8f856a9690e54fa7673
xiaoran <xiaoran.22@intl.zju.edu.cn>
2023-03-15 09:56:41 +0100

    variables

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,7 +3,14 @@ b = -118.24
 c = 116.39
 d = a-b
 e = c-a
-if d > e:
- print("Los Angeles")
+if d > e: # compare the two distances
+ print("Los Angeles") # the distance that	Rob travels to Los Angeles is larger.
 else:
- print("Haining")
+ print("Haining") # the distance that Rob travels to Haining is larger.
+#Rob traveled further to Haining
+X=True
+Y=False
+W=X and Y
+Z=X or Y
+print(W,Z)
+# W means false. Z means True.
